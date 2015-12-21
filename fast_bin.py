56f3193fcac3d6e11dd903c68e302e9e4efb66fe
kchang2 <kchang2@caltech.edu>
2015-12-22 02:22:55 +0800

    Finished fast_bin.py
    Can now specify the binning of runs to maximize usage of data volume.

--- fast_bin.py
+++ fast_bin.py
@@ -15,6 +15,7 @@ import fileinput
 import time
 import os
 import shutil
+import copy
 
 import parameters as p
 
@@ -25,7 +26,7 @@ retdir = os.getcwd()
 print "Current working directory %s" % retdir
 
 ## Ripping text file from runs_bin.txt ##
-os.chdir(".\necessary_documents\ ")
+os.chdir(retdir + "/necessary documents/")
 print "Grabbing data from %s" % os.getcwd()
 
 lsrun_total = []
@@ -38,28 +39,34 @@ print 'Begin reading appropriate ROOT binnings'
 with open('run_bins.txt') as file: #open txt file
     for num, line in enumerate(file, 1): #iterate through each line
         if endfound == False: #if END has not been found
-            if startfound == False: #if START has not been found
-                if "START" in line:
-                    startfound = True
-            else: #begin gathering data
-                if line != " ":
-                    lsrun.append(line[0:line.index(',')])
-                else:
-                    lsrun_total.append(lsrun)
-                    lsrun[:]
-            if "STOP" in line:
+            if "END" in line:
                 endfound = True
+            else:
+                if startfound == False: #if START has not been found
+                    if "START" in line:
+                        startfound = True
+                else: #begin gathering data
+                    if line != "\n":
+                        lsrun.append(line[0:line.index(',')])
+                    else:
+                        lsrun_total.append(copy.deepcopy(lsrun))
+                        lsrun[:] = []
+    lsrun_total.append(copy.deepcopy(lsrun)) #see if you can fix
 
 print 'End appropriate ROOT binnings'
 
+run_total = []
 #organizes the runlists to notation similar to that of the .list format
 for runs in lsrun_total:
+    run = []
     for indiv_run in runs:
-        indiv_run = indiv_run(:3) + "/" + indiv_run(3:)
+        run.append(indiv_run[:3] + "/" + indiv_run[3:])
+    run_total.append(copy.deepcopy(run))
+
 print 'Sucessfully structured to fit .list specifications'
 
 #gets the run number list
-for f in os.listdir():
+for f in os.listdir('.'):
     if p.runNumber in f:
         runlist = f
 
@@ -67,25 +74,32 @@ for f in os.listdir():
 lsrun_total_loc = []
 
 print 'Begin extracting from .list'
-for rootbin in lsrun_total:
+for rootbin in run_total:
     lsrun_loc = []
     with open(runlist) as file:
         for line in enumerate(file, 1):
-            if any(number in line for number in rootbin):
-                lsrun_loc.append(line)
-    lsrun_total_loc.append(lsrun_loc)
+            if any(number in line[1] for number in rootbin):
+                newline = line[1].replace("\n","")
+                lsrun_loc.append(copy.deepcopy(newline))
+    lsrun_total_loc.append(copy.deepcopy(lsrun_loc))
 print 'Successfully extract from .list'
 
 ### PLACING THE BINNING INTO ACTUAL FILL_.PY FILES ###
 # if files accessing are local or LXPLUS #
 if p.runFormat == 'L':
-    fileLocation = p.rootFileLocationLocal
+    if "output" in p.rootFileLocationLocal:
+        fileLocation = p.rootFileLocationLocal[:-7]
+    else:
+        fileLocation = p.rootFileLocationLocal
 else:
-    fileLocation = p.rootFileLocationLXPLUS
+    if "output" in p.rootFileLocationLXPLUS:
+        fileLocation = p.rootFileLocationLXPLUS[:-7]
+    else:
+        fileLocation = p.rootFileLocationLXPLUS
 
 # since this is originally for where the ROOT files are,
 # we want to do 1 step prior, so this gets us the python files
-os.chdir("..")
+os.chdir(fileLocation)
 fillLocation = os.getcwd() #python file location
 
 # Check current working directory.
@@ -95,39 +109,66 @@ print "Directory changed successfully %s" % retdir
 print 'Begin filling .py files'
 #reading each fill_.py within the fill folder
 
-listofFillspy = os.listdir()
+listofFillspy = os.listdir('.')
 listofFillspy.sort()
 
-with open(listofFillspy[0]) as file: #open txt file
-    for num, line in reversed(enumerate(file, 1)): #iterate through each line
-        if 'root://eoscms//eos/cms/store/data/' in line:
-            removeLine(file, num)
-            root_line = copy.deepcopy(num)
-        if 'EcalNtp' in line:
-            file.replace('EcalNtp_0.root','EcalNtp_')
+#with open(listofFillspy[0]) as file: #open txt file
+#    for num, line in reversed(enumerate(file, 1)): #iterate through each line
+#        if 'root://eoscms//eos/cms/store/data/' in line:
+#            removeLine(file, num)
+#            root_line = copy.deepcopy(num)
+#        if 'EcalNtp' in line:
+#            file.replace('EcalNtp_0.root','EcalNtp_')
+#
+#for line in reversed(open(listofFillspy[0]).readlines()):
+#    if 'root://eoscms//eos/cms/store/data/' in line:
+#        removeLine(file, num)
+#        root_line = copy.deepcopy(num)
+#    if 'EcalNtp' in line:
+#        file.replace('EcalNtp_0.root','EcalNtp_')
 
-for k in range(1,len(listofFillspy)):
-    os.remove(listofFillspy[k])
+with open(listofFillspy[0], 'r') as infile:
+    with open("fillEpsilonPlot_template.py", 'w') as outfile:
+        for line in infile:
+            if 'fileNames' in line:
+                line = line.replace("fileNames = cms.untracked.vstring(", "fileNames")
+            if 'root://eoscms//eos/cms/store/data/' in line:
+                infile.next()
+                continue
+            if 'EcalNtp' in line:
+                line = line.replace('EcalNtp_0.root','EcalNtp_')
+            outfile.write(line)
 
-os.rename(listofFillspy[0],"fillEpsilonPlot_template.py")
+print "removed all pre-exisiting root sources and specific EcalNtp_0.roots"
+
+for file in listofFillspy:
+    if "fillEpsilon" in file and file != 'fillEpsilonPlot_template.py':
+        os.remove(file)
+print "removed all files such that only the template is left"
 
 for bin in lsrun_total_loc:
-    insert = ""
-    for run in bin:
-        insert = insert + run +',\n' # will \n converts into actual new lines???
-    
     f = open("fillEpsilonPlot_template.py", 'r')
     filedata = f.read()
     f.close()
 
-    EcalNtp_index = 'EcalNtp_' + String(lsrun_total_loc.index(bin)) + '.root'
-    fileName = 'fillEpsilonPlot_iter_0_job_' + String(lsrun_total_loc.index(bin)) + '.py'
+    insert = "fileNames = cms.untracked.vstring(\n"
+    for run in bin:
+        insert = insert + "'" + run + "'"
+        if bin[-1] != run:
+            insert = insert + ',\n'
 
-    newfill = filedata.insert(root_line, insert)
-    newfill = filedata.insert('EcalNtp_',EcalNtp_index)
+    EcalNtp_index = 'EcalNtp_' + str(lsrun_total_loc.index(bin)) + '.root'
+    fileName = 'fillEpsilonPlot_iter_0_job_' + str(lsrun_total_loc.index(bin)) + '.py'
+
+    newfill = filedata.replace('EcalNtp_',EcalNtp_index)
+    newfill = newfill.replace('fileNames',insert)
 
     f = open(fileName, 'w')
     f.write(newfill)
     f.close()
 
+print "created new fill files"
+os.remove("fillEpsilonPlot_template.py")
+print os.listdir('.')
+print "deleted template file"
 
